cc2f34fa339eaa6e8c45bf1cc60caafe63af07e4
kevinqrt <kevinbrungard@gmx.de>
2023-12-10 13:05:54 +0100

    use openai services

--- dcb_backend/DCB_Backend/config.py
+++ dcb_backend/DCB_Backend/config.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
-from langchain.embeddings import HuggingFaceInstructEmbeddings
+#from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import HuggingFaceHub
+from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores import FAISS
 from django.conf import settings
+import os
 
 load_dotenv(dotenv_path=settings.BASE_DIR / ".env")
 
@@ -18,7 +21,19 @@ def get_vectorstore():
         
         return vectorstore
 
+
 STORECOMPONENT = FAISS
-EMBEDDINGS = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl") #OpenAIEmbeddings()
-LLM = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512}) # ChatOpenAI()
+
+#EMBEDDINGS = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl") #OpenAIEmbeddings()
+EMBEDDINGS = OpenAIEmbeddings(
+    openai_api_key=os.environ["OPENAI_API_KEY"],
+    model="text-embedding-ada-002",
+)
+
+#LLM = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512}) # ChatOpenAI()
+LLM = ChatOpenAI(
+    openai_api_key=os.environ["OPENAI_API_KEY"],
+    model_name="gpt-3.5-turbo",
+)
+
 VECTORSTORE = get_vectorstore()
