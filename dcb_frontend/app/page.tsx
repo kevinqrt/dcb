c9b5fb2bfcfd9e89acfb250500cf0db24732fe97
iamTim0 <90827363+iamTim0@users.noreply.github.com>
2023-12-11 19:37:04 +0100

    nextjs optimized

--- dcb_frontend/app/page.tsx
+++ dcb_frontend/app/page.tsx
@@ -1,17 +1,10 @@
-"use client"
-import MessageInput from './components/messageinput'
 import ChatContainer from './components/chatcontainer'
-import { useState } from 'react';
-import { buildMessages } from './utils/utils';
 
 export default function Home() {
-  const [messages, setMessages] = useState<Array<string>>([]);
   
   return (
     <main>
-      <ChatContainer input={<MessageInput setMessages={setMessages} messages={messages}/>}>
-        {buildMessages(messages)}
-      </ChatContainer>
+      <ChatContainer/>
     </main>
   )
 }
