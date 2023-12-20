b2a5edc3ca7cbe80c08804f03c79344b4b977713
kevinqrt <80759020+kevinqrt@users.noreply.github.com>
2023-12-20 18:34:07 +0100

    add max length

--- dcb_backend/chat/models.py
+++ dcb_backend/chat/models.py
@@ -4,8 +4,8 @@ from core.models import BaseModel, ClientModel
 
 
 class ChatModel(BaseModel):
-    question = models.CharField(max_length=500, null=False, blank=False)
-    response = models.CharField(max_length=500)
+    question = models.CharField(max_length=2000, null=False, blank=False)
+    response = models.CharField(max_length=2000)
     new_conversation = models.BooleanField()
     client = models.ForeignKey(ClientModel, on_delete=models.SET_NULL, null=True)
 
