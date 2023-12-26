20e574a3804bb594592a0e3b10e900becabf9cd5
kevinqrt <kevinbrungard@gmx.de>
2023-12-26 11:45:58 +0100

    Update update_repo.sh

--- dcb_frontend/update_repo.sh
+++ dcb_frontend/update_repo.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-sudo docker-compose down
+sudo docker-compose down nginx dcb_frontend
 git pull origin prod
-sudo docker-compose up --build
+sudo docker-compose up --build nginx dcb_frontend
