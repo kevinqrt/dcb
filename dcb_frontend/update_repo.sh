5ce4c33dbacbb476e05b0e8cd8f008ea27566158
EC2 Default User <ec2-user@ip-172-26-15-134.eu-central-1.compute.internal>
2023-12-26 11:10:52 +0000

    update script

--- dcb_frontend/update_repo.sh
+++ dcb_frontend/update_repo.sh
@@ -2,4 +2,4 @@
 
 sudo docker-compose down nginx dcb_frontend
 git pull origin prod
-sudo docker-compose --env-file ./.env.production up --build nginx dcb_frontend
+sudo docker-compose --env-file .env.production up --build nginx dcb_frontend
